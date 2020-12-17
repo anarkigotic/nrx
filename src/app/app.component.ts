e0c8f042acac401c9e7df27553ef5d5a6d89d71f
juan david jerez rubiano <JuanD.Jerez@Outsourcing.Libertycolombia.com>
2020-12-16 23:58:39 -0500

    test example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Laboratorios';
+  name = 'Juan';
+  firsName = 'Jerez';
 }
